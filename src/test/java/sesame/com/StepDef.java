d135d4febbb7e8fe40e394a6dfe55323d5fff929
Mohamed Amine Ben Safta <medaminedev23@gmail.com>
2023-12-09 20:36:14 +0100

    Code added to the files

--- src/test/java/sesame/com/StepDef.java
+++ src/test/java/sesame/com/StepDef.java
@@ -1,5 +1,39 @@
 package sesame.com;
 
-public class StepDef {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+public class StepDef {
+	public WebDriver driver;
+
+	@Given("I open google search page")
+	public void i_open_google_search_page() {
+		 System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+		 driver = new FirefoxDriver();
+		 driver.get("https://www.google.fr");
+	}
+	@When("I lookup the word {string}")
+	public void i_lookup_the_word(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("q")).clear();
+	    driver.findElement(By.name("q")).sendKeys(string);
+	    WebElement submit=driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"));
+	    JavascriptExecutor javax=(JavascriptExecutor)driver;
+	    javax.executeScript("scroll(0,200)");
+	    submit.click();
+	}
+	@Then("Search results display the word {string}")
+	public void search_results_display_the_word(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println(driver.getTitle().contains(string));
+		driver.close();
+	}
 }
