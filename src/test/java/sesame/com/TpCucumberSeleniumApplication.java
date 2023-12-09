c1b0ce35ac560b0089ac0aeb7e673a5690688a0f
MedAmineBS <133969417+MedAmineBS@users.noreply.github.com>
2023-12-09 22:03:54 +0100

    Update TpCucumberSeleniumApplication.java

--- src/test/java/sesame/com/TpCucumberSeleniumApplication.java
+++ src/test/java/sesame/com/TpCucumberSeleniumApplication.java
@@ -8,8 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"src/test/resources"},
-        glue = {""},
-        plugin = {"json:target/cucumber.json"}
+                plugin = {"json:target/cucumber.json"}
 		)
 public class TpCucumberSeleniumApplication {
 
